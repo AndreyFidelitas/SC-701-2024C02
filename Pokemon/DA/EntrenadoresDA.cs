122afa78e280b806992509b9f31bc84e8a34d773
Andrey Sanchez Zuñiga <osanchez60774@ufide.ac.cr>
2024-06-03 20:34:59 -0600

    Interfaces en la  clase de DA

---
+++ Pokemon/DA/EntrenadoresDA.cs
@@ -0,0 +1,23 @@
+﻿using Abstracciones.DA;
+using Abstracciones.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA
+{
+    public class EntrenadoresDA : IEntrenadoresDA
+    {
+        public IEnumerable<Entrenadores> ListarEntrenadores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entrenadores ListarEntrenadores(Guid Entrenador)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pokemon/DA/EquiposDA.cs
@@ -0,0 +1,23 @@
+﻿using Abstracciones.DA;
+using Abstracciones.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA
+{
+    public class EquiposDA : IEquiposDA
+    {
+        public IEnumerable<Equipos> ListarEquipos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Equipos ListarEquipos(Guid Equipos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
